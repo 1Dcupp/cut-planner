6fa972d6df28ed08d56f5a2894e1e57d9771c17c
1Dcupp <dcupp2018@icloud.com>
2026-04-28 19:38:16 -0400

    Update cut_planner.py

--- cut_planner.py
+++ cut_planner.py
@@ -1,7 +1,1 @@
-import streamlit as st
 
-st.title("DJ's Cut Planner")
-
-st.success("If you see this, everything is working.")
-
-st.write("Deployment is correct. We will rebuild features after this.")
